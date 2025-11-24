ffbd967e16756a9ab024a23c3c5a14cafe847e74
Jaisal Brar <jaisal.brar04@gmail.com>
2025-11-24 12:27:43 -0800

    04_Dialog_Boxes: Added alert, prompt, and confirm

---
+++ asp_frontend_dev/JavaScript/04_alert_prompt_confirm/script.js
@@ -0,0 +1,4 @@
+// const alertResult = alert("Are you sure you want to proceed?");
+// const isConfirmed = confirm("Would you like to continue?");
+const userPrompt = prompt("Please enter your name:")
+console.log(userPrompt);
