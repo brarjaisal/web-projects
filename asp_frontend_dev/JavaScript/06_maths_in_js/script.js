a86be29a09c72b4ef51c7c18839014d33158db21
Jaisal Brar <jaisal.brar04@gmail.com>
2025-11-24 14:44:41 -0800

    06_maths_in_js

---
+++ asp_frontend_dev/JavaScript/06_maths_in_js/script.js
@@ -0,0 +1,5 @@
+const width = +prompt("Enter the width:");
+const height = +prompt("Enter the height:");
+
+console.log(width * height);
+document.write(`The area of the rectangle is:  ${width * height}`);
