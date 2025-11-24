e66501ebf51d725d4ed3e951ab139959c66ff131
Jaisal Brar <jaisal.brar04@gmail.com>
2025-11-24 12:04:36 -0800

    03_code _execution_in_js

---
+++ asp_frontend_dev/JavaScript/03_code _execution_in_js/script.js
@@ -0,0 +1,9 @@
+debugger // used to pause the execution at this line
+console.log(firstName)
+
+var firstName = 'Akash';
+let lastName = 'Singh';
+let age = 15;
+const yearOfBirth = 1999;
+
+let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName;
