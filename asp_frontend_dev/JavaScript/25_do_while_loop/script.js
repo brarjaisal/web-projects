dc7b25b117b13ad925c888f835e5a89f2a24b10e
Jaisal Brar <jaisal.brar04@gmail.com>
2025-12-01 15:02:16 -0800

    25_do_while_loop

---
+++ asp_frontend_dev/JavaScript/25_do_while_loop/script.js
@@ -0,0 +1,40 @@
+// Using Do...While Loop
+
+console.log('Program Start')
+
+// let i = 0
+// while (i < 5) {
+//   console.log(i)
+//   i++
+// }
+
+// for(let i = 0; i < 5; i++) { 
+//     console.log(i);
+// }
+
+let i = 0 
+do {
+    console.log(i);
+    i++
+} while (i < 5)
+
+console.log('Program Ended')
+
+// Output:
+// Program Start
+// 0
+// 1
+// 2
+// 3
+// 4
+// Program Ended
+
+// Explanation:
+// The code demonstrates the use of a do...while loop in JavaScript. 
+// It initializes a variable 'i' to 0 and then enters the do block,
+// where it prints the value of 'i' and increments it by 1. 
+// The loop continues to execute as long as 'i' is less than 5. 
+// This ensures that the block of code runs at least once before checking the condition.    
+
+// for loop and while loop are also shown as comments for comparison.
+
