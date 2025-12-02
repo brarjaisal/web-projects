95b8596be543a0aaf3db835fa30bd99cf950484b
Jaisal Brar <jaisal.brar04@gmail.com>
2025-12-02 13:13:41 -0800

    28_execution_context

---
+++ asp_frontend_dev/JavaScript/28_execution_context/script.js
@@ -0,0 +1,34 @@
+// Global Execution Context in Javascript
+// Memory creation Phase :
+// username = undefined
+
+// Code execution Phase :
+// username = "Jaisal"
+
+
+// console.log(firstName)
+
+// var firstName = 'Jaisal'
+// let lastName = 'Brar'
+// let age = 29
+// const yearOfBirth = 1996
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
+
+debugger
+sayHi()
+
+const username = "Jaisal"
+const userAge = 29
+
+function sayHi() {
+    const a = 14
+    const b = 12
+    add(7, 9)
+}
+
+function add(x, y) {
+    return x + y
+}
+
+console.log("Program Ended!")
