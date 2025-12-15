e0d2b167e14a61bb4f16cd96e4fdf78dc4bbf3d2
Jaisal Brar <jaisal.brar04@gmail.com>
2025-12-15 11:39:19 -0800

    updated_change_styles_using_js

--- asp_frontend_dev/JavaScript/53_how_to_change_styles_using_js/script.js
+++ asp_frontend_dev/JavaScript/53_how_to_change_styles_using_js/script.js
@@ -19,8 +19,24 @@ const allLinks = document.querySelectorAll('a')
 
 
 for (const link of allLinks) {
-    link.style.color = 'teal'
-    link.style.textDecoration = 'none'
-    link.style.fontWeight = '700'
-    link.style.fontFamily = 'cursive'
-}
+//     link.style.color = 'teal'
+//     link.style.textDecoration = 'none'
+//     link.style.fontWeight = '700'
+//     link.style.fontFamily = 'cursive'
+//     link.style.fontSize = '18px'
+
+// link.style.cssText = `
+//         color: teal;
+//         text-decoration-line: none;
+//         font-weight: 700;
+//         font-family: cursive;
+//         font-size: 18px;
+//         `
+
+// link.className = 'green-link'
+link.setAttribute('class', 'green-link')
+
+link.classList.add('green-link') 
+link.classList.remove('my-list')
+// link.classList.toggle('my-link')
+}        
